a77533cd503cc1d825fab4dcd4b6e712cf381089
AlexStoskyi <95970452+AlexStoskyi@users.noreply.github.com>
2024-12-01 18:47:52 +0100

    rename

---
+++ tests/C001_Login_with_valid_email.spec.ts
@@ -0,0 +1,55 @@
+import { expect, test } from "../fixtures/fixtures";
+import successMsg from "../constants/successMsg.json";
+
+test.beforeEach(async ({ page, baseURL }) => {
+  await page.goto(`${baseURL!}` + "/Login");
+});
+
+test.describe("Tests for Skillibrium Learn", () => {
+  test("Edit the user profile", async ({
+    homePage,
+    baseURL,
+    loginPage,
+    mailAPI,
+    basePage,
+    emailPage,
+  }) => {
+    const userEmail = process.env.USER_EMAIL!;
+
+    test.step("Login with valid email", async () => {
+      await loginPage.fillEmailInput(userEmail);
+      await loginPage.clickOnSendMagicLinkBtn();
+
+      const html = await mailAPI.getMsgHtmlBodyBySubject(
+        userEmail,
+        `Confirmation email for ${userEmail}`,
+        { attemptTimeout: 10000 }
+      );
+
+      await basePage.openHTML(html.replaceAll('target="_blank"', ""), {
+        htmlTitle: `Confirmation email for ${userEmail}`,
+      });
+
+      await emailPage.clickBtnLoginTo();
+      await loginPage.fillEmailInput(userEmail);
+      await loginPage.clickOnSendMagicLinkBtn();
+    });
+
+    await expect(loginPage.successPopUp).toBeVisible();
+
+    const html = await mailAPI.getMsgHtmlBodyBySubject(
+      userEmail,
+      `Confirmation email for ${userEmail}`,
+      { attemptTimeout: 10000 }
+    );
+    await basePage.openHTML(html.replaceAll('target="_blank"', ""), {
+      htmlTitle: `Confirmation email for ${userEmail}`,
+    });
+
+    await test.step('Click the "Login to Skillibrium" button', async () => {
+      await emailPage.clickBtnLoginTo();
+      await expect(homePage.userDropDownBtn).toBeVisible();
+      await expect(loginPage.page).toHaveURL(`${baseURL}` + "/Home");
+    });
+  });
+});
